7b5c8911babd3ee5ccffa71b8b67af1335e99562
silvia <alumno@isbo.edu.uy>
2024-06-14 19:27:38 -0300

    en el archivo pruebafunciones.php agrego la funcion Mostrar producto por modelo

--- pruebafunciones.php
+++ pruebafunciones.php
@@ -13,7 +13,7 @@ function agregarProducto($nombre, $cantidad, $valor, $modelo) {
     return $producto;
 }
 
-function buscarProductoPorModelo($producto, $modelo) {
+function buscarProductoPorModelo($productos, $modelo) {
     foreach ($productos as $producto) {
         if ($producto['modelo'] == $modelo) {
             return "Nombre: " . $producto['modelo'] . "<br>";
@@ -21,4 +21,43 @@ function buscarProductoPorModelo($producto, $modelo) {
     }
     return "Producto no encontrado.<br>";
 }
+
+function mostrarProductos($productos) {
+    $result = '';
+    foreach ($productos as $producto) {
+        $result .= "Nombre: " . $producto['nombre'] . 
+        ", cantidad: " . $producto['cantidad'] . 
+        "valor: " . $producto['valor'] .
+        "modelo: " . $producto['modelo'] ."<br>";
+        
+   
+    }
+    return $result;
+}
+
+
+
+
+
+
+
+// Agregar algunos productos de ejemplo
+$productos = agregarProducto("Laptop", 1, 500, "X123");
+$productos = agregarProducto("Celular", 2, 200, "Y987");
+$productos = agregarProducto("Tablet", 1, 350, "Z345");
+
+// Mostrar los productos agregados
+echo "<pre>";
+print_r($productos);
+echo "</pre>";
+
+
+// Buscar un producto por modelo específico
+$modeloABuscar = "X123";
+$productoEncontrado = buscarProductoPorModelo($productos, $modeloABuscar);
+
+// Mostrar el resultado de la búsqueda
+echo $productoEncontrado;
+
+
 ?>
